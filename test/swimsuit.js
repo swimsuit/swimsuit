a7d8f858025e4ec1b06133c532522dccf47a055c
Ed Moore <eddie.moore@gmail.com>
2015-06-30 14:53:24 +0800

    testing errors

--- test/swimsuit.js
+++ test/swimsuit.js
@@ -10,6 +10,13 @@ describe('Swimsuit', () => {
     })
   })
 
+  it('should reject if trying to load a number', done => {
+    Swimsuit.load(42).catch(err => {
+      expect(err).to.exist
+      expect(err).to.equal('You must pass in an array of tests')
+    })
+  })
+
   it('should return a promise', () => {
     expect(Swimsuit.websockets instanceof Promise).to.be.true
   })

de2b89b682df7b2a781a5b37d4a891521feae0cb
Ed Moore <eddie.moore@gmail.com>
2015-06-30 15:32:40 +0800

    fixing tests

--- test/swimsuit.js
+++ test/swimsuit.js
@@ -4,17 +4,17 @@ import Swimsuit from '..'
 import websockets from '../src/feature-tests/websockets'
 
 describe('Swimsuit', () => {
-  it('should load additional feature tests', done => {
+  it('should use additional feature tests', done => {
     Swimsuit.use(websockets).then(() => {
       expect(Swimsuit.websockets).to.exist
       done()
     })
   })
 
-  it('should reject if trying to load a number', done => {
+  it('should reject if trying to use a number', done => {
     Swimsuit.use(42).catch(err => {
       expect(err).to.exist
-      expect(err).to.equal('You must pass in an array of tests')
+      expect(err).to.equal('You must pass in an object or array of objects')
       done()
     })
   })
